08cc836e7e46a105841f43c0e8bcbb429564d9a9
Job <bogach816@example.com>
2023-07-21 10:40:45 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+    <div className='menu'>
+        <button type='button'><img src="src/img/hambergermenu.svg" alt="" /></button>
+        <p className='test-title'>Test Task</p>
+        <a href="#" className=""><img src="src/img/instagram.svg" alt="" /></a>
+    </div>
+    </div>
+  )
+}
+
+export default Header
